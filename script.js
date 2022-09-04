5e9f96a3bf7f9159c31f4c69097fda43c93e5068
Yunus Ahmadov <yunus.ahmedov.11@mail.ru>
2022-09-04 21:37:09 +0400

    volvo

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+let scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// let bb=document.querySelector('to_top')
+
+// if(document.documentElement.scrollTop>3600){
+//     bb.style.display="block"
+// }
+// console.log(document.documentElement.scrollTop);
+
